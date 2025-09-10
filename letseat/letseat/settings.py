5efb8a6e950fdc1ba5d84e535a8470ae1e3b133c
chintakumar <kumar.chinta10021@gmail.com>
2025-09-10 15:24:01 +0530

    Update settings.py

--- letseat/letseat/settings.py
+++ letseat/letseat/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path,os
+from pathlib import Path
 from decouple import config, Csv
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
